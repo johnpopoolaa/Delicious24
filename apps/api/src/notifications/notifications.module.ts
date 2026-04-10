1266ff456511abec6c870bc76778c036a322689b
John <johnpops59@gmail.com>
2026-04-10 01:29:53 +0100

    feat(notifications): install twilio, wire PrismaModule and TWILIO_CLIENT provider

--- apps/api/src/notifications/notifications.module.ts
+++ apps/api/src/notifications/notifications.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
+import twilio, { Twilio } from 'twilio';
+import { PrismaModule } from '../prisma/prisma.module';
 import { NotificationSenderService } from './notification-sender.service';
 
 @Module({
-  providers: [NotificationSenderService],
+  imports: [PrismaModule],
+  providers: [
+    NotificationSenderService,
+    {
+      provide: 'TWILIO_CLIENT',
+      useFactory: (): Twilio =>
+        twilio(process.env.TWILIO_ACCOUNT_SID!, process.env.TWILIO_AUTH_TOKEN!),
+    },
+  ],
   exports: [NotificationSenderService],
 })
 export class NotificationsModule {}

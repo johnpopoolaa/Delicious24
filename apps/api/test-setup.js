cf0f2ef6da272b6f24e333cd042cb77aca2d1a15
John <johnpops59@gmail.com>
2026-04-15 23:57:36 +0100

    test(Api): add jest setupFiles and Twilio env stubs for unit tests

---
+++ apps/api/test-setup.js
@@ -0,0 +1,5 @@
+// Set required environment variables for unit tests
+process.env.TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID || 'ACtest00000000000000000000000000000';
+process.env.TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN || 'test_auth_token_00000000000000000';
+process.env.TWILIO_SMS_FROM = process.env.TWILIO_SMS_FROM || '+15550000001';
+process.env.TWILIO_WHATSAPP_FROM = process.env.TWILIO_WHATSAPP_FROM || '+15550000002';
